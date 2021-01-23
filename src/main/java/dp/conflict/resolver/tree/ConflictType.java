42e5481739f0ae07a4e21bc3d6b17cbd8653beb7
Fabian Oraze <oraze.fabian@icloud.com>
2021-01-23 18:35:13 +0100

    added new ConflictType enum (TYPE_3) for the case when all leaf nodes that are not also root nodes pose a potential threat and should be analyzed

--- src/main/java/dp/conflict/resolver/tree/ConflictType.java
+++ src/main/java/dp/conflict/resolver/tree/ConflictType.java
@@ -4,8 +4,8 @@ public enum ConflictType {
     /**
      * Different conflict(issue) types, that can occur in the finished {@link CallTree}
      * <p>
-     * TYPE_1: an object is directly in use by two different versions of the same dependency
-     * TYPE_2: a method is called at least twice with different signatures from two distinct versions af a dependency
+     * TYPE_1: a method is called at least twice with different signatures from two distinct versions af a dependency
+     * TYPE_2: an object is directly in use by two different versions of the same dependency
      * TYPE_3: all leaf nodes are critical and must be solved with clingo
      */
     TYPE_1,
