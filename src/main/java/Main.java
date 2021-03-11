efd7d92b69cbe0b34f8a910d237ec8f1e9177f37
Fabian Oraze <oraze.fabian@icloud.com>
2021-03-11 15:53:10 +0100

    added string param to be the path to the maven project to be analyzed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,34 +16,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String test = "/Users/fabian/Projects/Sample/runtime_conflict_sample/Project_A/";
-        String target = "/Users/fabian/Projects/Sample/fastjson/";
-        String curr = "/Users/fabian/Projects/automized-DP-conflict-resolver/automized-dp-conflict-resolver/";
-        String jar = "/Users/fabian/.m2/repository/org/runtime/conflict/Project_B/1.0/Project_B-1.0.jar";
-
-        //String param = args[0];
-
+        String param = args[0];
 
         long time = System.currentTimeMillis();
         Tree tree;
         AnswerObject answer = new AnswerObject();
         CentralMavenAPI.setMaxVersionsNumFromCmr(5);
         try {
-            tree = new CallTree(curr, answer);
+            File output = new File("target/output.txt");
+            output.createNewFile();
+            FileWriter writer = new FileWriter(output);
+
+            tree = new CallTree(param, answer);
             tree.computeCallTree();
             FactBuilder parser;
             parser = new FactBuilder(tree.getConflicts(ConflictType.TYPE_3), tree.getNeededJars());
             answer.setIDMap(parser.getIdMap());
             answer.solve();
             long currTime = (System.currentTimeMillis() - time) / 1000 / 60;
-            System.out.println("Needed time: " + currTime + " min");
+            String timeInfo = "Needed time: " + currTime + " min";
             System.out.println("Possible jar configurations: " + answer.getAnswers());
+            System.out.println(timeInfo);
 
-            File output = new File("target/output.txt");
-            output.createNewFile();
-            FileWriter writer = new FileWriter(output);
+            writer.write(timeInfo + "\n");
+
+            writer.write("\nCould not find usage for following dependencies: (bloated)\n");
+            for (String bloated : answer.getBloatedJars()) {
+                writer.write(bloated + "\n");
+            }
+
             for (List<String> configuration : answer.getAnswers()) {
-                writer.write("Possible dependency configuration:\n");
+                writer.write("\nPossible dependency configuration:\n");
                 for (String dependency : configuration) {
                     writer.write(dependency + "\n");
                 }
