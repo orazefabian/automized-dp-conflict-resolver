7c9777c8d7a5a797d28b72421f11a76a7d268f48
Fabian Oraze <oraze.fabian@icloud.com>
2021-03-25 08:37:22 +0100

    added empty env file

---
+++ src/main/java/dp/api/database/EnvConfig.java
@@ -0,0 +1,14 @@
+package dp.api.database;
+
+/*********************************
+ Created by Fabian Oraze on 12.02.21
+ *********************************/
+
+public class EnvConfig {
+
+    public static String DB_URL = "";
+    public static String USER = "";
+    public static String PASSWORD = "";
+
+
+}
